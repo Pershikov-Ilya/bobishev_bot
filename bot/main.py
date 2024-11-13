bcaa6c6c203ceb43c3a133a2b831975d8e02e439
Ilya Pershikov <kartox_a@mail.ru>
2024-11-13 18:12:15 +0300

    болванка

---
+++ bot/main.py
@@ -0,0 +1,28 @@
+import asyncio
+
+from aiogram.methods import DeleteWebhook
+from aiogram.types import BotCommand
+from aiogram import Bot, Dispatcher
+
+import config as conf
+
+
+async def main() -> None:
+
+    bot = Bot(token=conf.BOT_TOKEN)
+    dp = Dispatcher()
+    # dp.include_router(router)
+    bot_commands = [
+                BotCommand(command="/start", description="Get start")]
+
+    await bot.set_my_commands(bot_commands)
+    await bot(DeleteWebhook(drop_pending_updates=True))
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    try:
+        print("on")
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("off")
